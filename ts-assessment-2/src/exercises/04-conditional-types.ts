2a431a075a49fb72b6d63dc57ef16f74f9b27a5d
mpho09 <mphomangena0409@gmail.com>
2026-07-01 14:30:24 +0200

    fixed the 04-conditional-types

--- ts-assessment-2/src/exercises/04-conditional-types.ts
+++ ts-assessment-2/src/exercises/04-conditional-types.ts
@@ -11,7 +11,7 @@
  * Use a conditional type with `infer`. */
 
 // TODO: T extends (infer U)[] ? U : T
-export type Flatten<T> = ___;
+export type Flatten<T> = T extends (infer U)[] ? U : T;
 
 // Compile-time checks (these are type-level assertions):
 export const f1: Flatten<string[]> = "hello";   // must be string
@@ -23,7 +23,7 @@ export const f2: Flatten<number> = 42;          // must be number
  *   UnwrapPromise<string> = string */
 
 // TODO: T extends Promise<infer U> ? U : T
-export type UnwrapPromise<T> = ___;
+export type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 
 export const u1: UnwrapPromise<Promise<boolean>> = true;   // boolean
 export const u2: UnwrapPromise<string> = "x";              // string
@@ -36,7 +36,7 @@ export const u2: UnwrapPromise<string> = "x";              // string
  *     = { a: string; b: number } */
 
 // TODO: { [K in keyof T]: NonNullable<T[K]> }
-export type NonNullableFields<T> = ___;
+export type NonNullableFields<T> = { [K in keyof T]: NonNullable<T[K]> };
 
 type Raw = { a: string | null; b: number | undefined };
 export const clean: NonNullableFields<Raw> = { a: "ok", b: 5 };
